542738bf4df67b057cb25112134db37ec696524a
Fabian <fabian.eichfeldt@gmx.de>
2025-10-23 16:44:43 +0200

    add postcodes to stats

--- scripts/geo.js
+++ scripts/geo.js
@@ -1,23 +1,34 @@
 import * as axios from 'axios';
-import { trails } from '../js/data/trails.js';
+import { getTrails } from '../js/data/trails.js';
 
-const api_key = process.env.API_KEY;
-async function getState(lat, lon) {
+const api_key = process.env.GEO_API_KEY;
+async function getDetails(lat, lon) {
   const response = await axios.default.get(`https://api.geoapify.com/v1/geocode/reverse?lang=de&lat=${lat}&lon=${lon}&format=json&apiKey=${api_key}`);
-  return response.data.results[0].state;
+  return {
+    state: response.data.results[0].state,
+    postCode: response.data.results[0].postcode
+  }
 }
 
 (async () => {
   const states = [];
+  const postCodes = [];
+  const trails = await getTrails();
   for (const trail of trails) {
-    const state = await getState(trail.coords[0], trail.coords[1]);
+    const {state, postCode} = await getDetails(trail.latitude, trail.longitude);
     states.push(state);
-    console.log(`Trail: ${trail.name} => ${state}`);
+    postCodes.push(postCode);
+    console.log(`Trail: ${trail.name} => ${state}, ${postCode}`);
   }
 
-    const counts = states.reduce((acc, state) => {
+    const countStates = states.reduce((acc, state) => {
         acc[state] = (acc[state] || 0) + 1;
         return acc;
     }, {});
-    console.log(counts);
+    const countPostCodes = postCodes.reduce((acc, postCode) => {
+      acc[postCode] = (acc[postCode] || 0) + 1;
+      return acc;
+  }, {});
+    console.log(countStates);
+    console.log(countPostCodes);
 })();
