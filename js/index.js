bfa0395970a7c4ffa029dfd1d43c546c07bbf866
Fabian <fabian.eichfeldt@gmx.de>
2025-11-26 11:55:42 +0100

    do not add referer with typo

--- js/index.js
+++ js/index.js
@@ -78,7 +78,7 @@ function pageCounter() {
     headers: {
       "Content-Type": "application/json",
       "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml4YWZlZ214a2FkYnpoeG1lcHNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2Mjc1MzAsImV4cCI6MjA3NjIwMzUzMH0.BRbdccgrW7aZpvB_S4_qKn_BRcfPMyWjQAVuVuy2wyQ",
-      "referrer": document.referrer,
+      // "referrer": document.referrer,
     },
   });
 }
