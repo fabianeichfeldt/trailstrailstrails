9f316a40edb1000526e44c2bd3101f95d8544a95
Fabian <fabian.eichfeldt@gmx.de>
2026-05-17 18:41:15 +0200

    fix add-visits

--- plugins/visit.client.ts
+++ plugins/visit.client.ts
@@ -4,7 +4,12 @@ export default defineNuxtPlugin(() => {
   trackVisit(window.location.pathname, document.referrer)
 
   const router = useRouter()
+  let initialNavigationDone = false
   router.afterEach((to) => {
+    if (!initialNavigationDone) {
+      initialNavigationDone = true
+      return
+    }
     trackVisit(to.path, window.location.href)
   })
 })
