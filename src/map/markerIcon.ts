739056a5613f104c1775824dd43a8915b40dff04
Fabian <fabian.eichfeldt@gmx.de>
2026-05-17 16:15:16 +0200

    improve markers

--- src/map/markerIcon.ts
+++ src/map/markerIcon.ts
@@ -3,7 +3,6 @@ export interface MarkerIconOptions {
   iconSize: [number, number]
   iconAnchor: [number, number]
   popupAnchor: [number, number]
-  shadowUrl: string
   className: string
 }
 
@@ -14,11 +13,10 @@ export function markerIconOptions(type: string, approved: boolean): MarkerIconOp
     approved           ? 'verified' : 'unverified'
 
   return {
-    html: `<div class="marker-wrapper marker-${category}"><img src="https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png" class="marker-img" /></div>`,
-    iconSize:    [25, 41],
-    iconAnchor:  [12, 41],
-    popupAnchor: [1, -34],
-    shadowUrl:   'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+    html:        `<div class="map-pin map-pin-${category}"></div>`,
+    iconSize:    [18, 18],
+    iconAnchor:  [9, 22],
+    popupAnchor: [0, -24],
     className:   '',
   }
 }
